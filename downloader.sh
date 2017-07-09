a2ca7377b7b0d79ccee1c978789ca1a90f75c892
Juliano Efson Sales <julianoens@gmail.com>
2017-07-09 23:35:35 +0200

    alpha (not fully tested) version

--- downloader.sh
+++ downloader.sh
@@ -9,30 +9,39 @@ if [ ! $1 ]; then
 	echo "Load Indra models from $BASEURL."
 	echo "Usage: downloader.sh <name_of_the_model>"
 	echo "Example to load the public Word2Vec model of Google News"
-	echo " downloader.sh w2v-en-GoogleNews300neg"
+	echo " downloader.sh w2v-en-googlenews300neg"
 	echo ".........................................................."
 	exit 0
 fi
 
-loaded=`docker exec -it indramongo mongo $1 --quiet --eval "db.getCollectionNames().length"`
-loaded=$(echo -n "${loaded//[[:space:]]/}")
 
-if [ "$loaded" != "0" ] ; then
-	echo "$1 already loaded."
-	exit 0
+if [ $1 == w2v* ] || [ $1 == glove* ] || [ $1 == lsa* ]; then
+	MODELFILE="$1.annoy.tar.gz"
+	MD5FILE="$1.annoy.tar.gz.md5"
+	$ISANNOY = 1;
+	mkdir -p annoy/data
+	cd annoy
+else
+
+	loaded=`docker exec -it indramongo mongo $1 --quiet --eval "db.getCollectionNames().length"`
+	loaded=$(echo -n "${loaded//[[:space:]]/}")
+
+	if [ "$loaded" != "0" ] ; then
+		echo "$1 already loaded."
+		exit 0
+	fi
+
+	MODELFILE="$1.tar.gz"
+	MD5FILE="$1.tar.gz.md5"
+	$ISANNOY = 0;
+	mkdir -p mongo/data
+	cd mongo
 fi
 
-
-
-MODELFILE="$1.tar.gz"
-MD5FILE="$1.tar.gz.md5"
 MODELURL="$BASEURL/$MODELFILE"
 MD5URL="$BASEURL/$MD5FILE"
 
-mkdir -p dumps/data 
-cd dumps
-
-if [ ! -d "./data/$1" ]; then
+if [ ! -d "./$1" ]; then
 	echo "Downloading $MODELURL .."
 	wget -nc $MODELURL && wget -nc $MD5URL && md5sum -c $MD5FILE 
 	echo "Extracting $MODELFILE"
@@ -40,7 +49,9 @@ if [ ! -d "./data/$1" ]; then
 	rm $MODELFILE $MD5FILE
 fi
 
-docker exec -it indramongo mongorestore /dumps/data/$1 -d $1 --stopOnError
+if [! $ISANNOY]; then
+	docker exec -it indramongo mongorestore /dumps/data/$1 -d $1 --stopOnError
+fi
 
 echo "Finished."
 
